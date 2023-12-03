0e01010dfdbc2d0782f5e7a664d79d7a88dc2103
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-03 13:37:15 +0530

    Update _about.py

--- _about.py
+++ _about.py
@@ -5,6 +5,6 @@ def main():
     st.title("About Us")
 
 
-    st.text("This is web app for internal use case for Aangan Trust.")
+    st.text("This is web app for Sentiment Analysis.")
 
-    st.text("This app was developed to translate the multilingual survey comments into English.")
+    st.text("This app was developed to analyse sentiment of the stament provided along with the score.")
